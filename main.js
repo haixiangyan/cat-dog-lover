ff44ebff5b94c27ad2695906c9539c3e6d98dda5
Haixiang <haixiang6123@gmail.com>
2020-05-23 11:13:06 +0800

    添加注释

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-const catImage = 'img/cat.jpg'
-const dogImage = 'img/dog.jpg'
-const loverImage = 'img/lover.jpg'
+const $imgs = Array.from(document.querySelectorAll('img'))
 
-const images = [catImage, dogImage, loverImage]
-
-const $image = document.querySelector('#image')
-
+let start = false
 let intervalId = null
+let curt = 0
 
+// 开始游戏
 const startGame = () => {
-  let curt = 0
   // 不断更新
   return setInterval(() => {
-    // 更新图片
-    $image.src = images[curt]
+    // 隐藏上一张
+    $imgs[curt].style.opacity = '0'
+
     // 下一张
-    curt = (curt + 1) % images.length
-  }, 1000)
+    curt = (curt + 1) % $imgs.length
+
+    // 显示下一张
+    $imgs[curt].style.opacity = '1'
+  }, 20)
 }
 
+// 结束游戏
 const endGame = () => {
   window.clearInterval(intervalId)
 }
 
-let start = false
+// 按下任意按钮开始/结束游戏
 document.onkeydown = () => {
   if (start) {
     endGame()
@@ -33,3 +34,9 @@ document.onkeydown = () => {
 
   start = !start
 }
+
+// 显示第一个图片
+const [$cat] = $imgs
+$cat.style.opacity = '1'
+
+alert('按下空格开始和结束，看你做猫，做狗还是做情人 :)')
